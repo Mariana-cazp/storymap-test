0867ef4e31a59747dbb0e1edcc1d3f2b7efe3c72
Mariana-cazp <mcastroa7@gmail.com>
2026-02-25 14:02:56 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRuZB1ZB5gDOUOA2iADuEUfTEcpkW_vIAy6RKYrxqPeFG8mlZwq3GGM9ZmrDEpnqVrZu51S8ln3lPQF/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pE49uopNpD-BMmXG5namcGehiezkGP1V_L-RIOupEpo/edit?gid=1033046795#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
